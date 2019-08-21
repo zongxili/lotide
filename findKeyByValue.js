b8d1947e030722bbe17283f45dad087fc9f4fb03
Zongxi Li <zongxiliwork@gmail.com>
2019-08-21 22:22:19 +0000

    this leaves an error but this is about a name in testing file

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -6,18 +6,24 @@ const assertEqual = function(actual, expected) {
     console.log(`🔴 🔴 🔴 Assertion Failed: [ ${actual} ] !== [ ${expected} ]`);
 };
 
-const findKeyByValue = function(inputObject, inputValue){
-  let outputKey = "";
-  console.log(Object.values(inputObject));
+const findKeyByValue = function(inputObject, inputValue) {
+  //here make it undefined, in case there is no matching
+  let outputKey = undefined;
+  //make Key name as an array to check
+  let keyArr = Object.keys(inputObject);
+  for (let keyName of keyArr) {
+    if (inputValue === inputObject[keyName])
+      outputKey = keyName;
+  }
   return outputKey;
 };
 
 //testing cases
-const bestTVShowsByGenre = { 
-    sci_fi: "The Expanse",
-    comedy: "Brooklyn Nine-Nine",
-    drama:  "The Wire"
-  };
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
   
-  assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-  assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
